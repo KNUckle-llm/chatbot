98335373784eba7f4dd0582d6f261a422fc339a5
Jinseong <hdss8234@gmail.com>
2025-09-21 03:09:20 +0900

    test: api 테스트 작성

---
+++ tests/api/test_api.py
@@ -0,0 +1,18 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+from app.core.config import settings
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": "ok",
+        "server_name": settings.APP_NAME,
+        "version": settings.APP_VERSION,
+        # timestamp는 동적으로 생성되므로 값만 확인
+        "timestamp": response.json()["timestamp"]
+    }
