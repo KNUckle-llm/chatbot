2b57bf5bdb7f15bbc58c8ec24e6e7b8d22357f80
Jinseong <hdss8234@gmail.com>
2025-10-11 00:00:51 +0900

    dev, prod 환경 분리

--- src/core/config.py
+++ src/core/config.py
@@ -1,13 +1,77 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+from pydantic import BaseModel
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+    YamlConfigSettingsSource,
+)
+
+ROOT = Path(__file__).resolve().parents[2]
+YAML_PATH = ROOT / "configs" / "app"
+
+load_dotenv(ROOT / ".env")
+env = os.getenv("APP_ENV")
+
+
+class AppConfig(BaseModel):
+    name: str
+    version: str
+
+
+class ChatConfig(BaseModel):
+    provider: str
+    model: str
+    temperature: float
+    retry: int
+
+
+class EmbeddingConfig(BaseModel):
+    model: str
+
+
+class LoggingConfig(BaseModel):
+    level: str = "INFO"
 
 
 class AppSettings(BaseSettings):
-    APP_NAME: str = "KNUC API Server"
-    APP_VERSION: str = "0.1.0"
+    APP_ENV: str | None = "dev"
     OPENAI_API_KEY: str | None = None
-    VECTORSTORE_DIR: str | None = None
 
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    # YAML 설정
+    app: AppConfig | None = None
+    llm: ChatConfig | None = None
+    embedding: EmbeddingConfig | None = None
+    logging: LoggingConfig | None = None
+
+    model_config = SettingsConfigDict(env_file=ROOT / ".env", extra="ignore")
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        yaml_settings = YamlConfigSettingsSource(
+            settings_cls, yaml_file=YAML_PATH / f"{env}.yaml"
+        )
+        base_yaml_settings = YamlConfigSettingsSource(
+            settings_cls, yaml_file=YAML_PATH / "base.yaml"
+        )
+        return (
+            init_settings,
+            env_settings,
+            dotenv_settings,
+            yaml_settings,
+            base_yaml_settings,
+            file_secret_settings
+        )
 
 
 settings = AppSettings()
