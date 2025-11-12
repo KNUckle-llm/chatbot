15e29c162e95a08a89b0933fa5038d6197589437
ChanGyuLee <lay5137@naver.com>
2025-11-12 15:19:34 +0900

    health_check 앤드포인트 수정

--- tests/api/test_main.py
+++ tests/api/test_main.py
@@ -6,4 +6,9 @@ client = TestClient(app)
 def test_health_check():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"status": "ok"}
+    json_data = response.json()
+    
+    assert json_data["status"] == "ok"
+    assert "server_name" in json_data
+    assert "version" in json_data
+    assert "timestamp" in json_data
